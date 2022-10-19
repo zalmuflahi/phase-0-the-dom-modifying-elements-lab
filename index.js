e215837ec25ff853829d23dd3190a674eeb12dde
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-19 06:59:29 -0400

    commit tree adbad83069c5906f913d3f9db7d3743aac8f74ed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const re = document.getElementById('main') 
+re.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Z is the champion"
+
